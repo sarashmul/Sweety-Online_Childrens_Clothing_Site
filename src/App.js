35216307bb14365f005c6d0ca76fdfe5874db91d
Your Name <you@example.com>
2025-04-28 20:02:29 +0300

    style boostrap to navBar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-
 import './App.css';
-
 import NavBar from './component/Global/NavBar';
 import Footer from './component/Global/Footer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './component/screens/HomePage';
+import ProductsPage from './component/screens/ProductsPage';
+import ShoppingCartPage from './component/screens/ShoppingCartPage';
+import CheckoutPage from './component/screens/CheckoutPage';
+
 
 function App() {
+  const routes = {
+    home: "/",
+    product: "/product",
+    ShoppingCart: "/ShoppingCart",
+    Checkout: "/Checkout"
+  };
+
   return (
     <Provider store={store}>
-   <div className="app-container">
-    <NavBar/>
-    <Footer/>
-   </div>
-   </Provider>
+      <BrowserRouter>
+        <div className="app-container">
+          <NavBar />
+          
+      
+          <Routes>
+            <Route path={routes.home} element={<HomePage />} />
+            <Route path={routes.product} element={<ProductsPage />} />
+            <Route path={routes.ShoppingCart} element={<ShoppingCartPage />} />
+            <Route path={routes.Checkout} element={<CheckoutPage />} />
+          </Routes>
+
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/component/Global/NavBar.js
+++ src/component/Global/NavBar.js
@@ -1,35 +1,52 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
- import HomePage from '../screens/HomePage';
- import ProductsPage from '../screens/ProductsPage';
-import ShoppingCartPage from '../screens/ShoppingCartPage';
-import CheckoutPage from '../screens/CheckoutPage';
-export default function NavBar() {
+import React from 'react';
+import {Link } from 'react-router-dom';
 
-    const routs={
-        home:"/",
-        product:"/product",
-        ShoppingCart:"/ShoppingCart",
-        Checkout:"/Checkout"
-    }
+export default function NavBar() {
+  const routes = {
+    home: "/",
+    product: "/product",
+    ShoppingCart: "/ShoppingCart",
+    Checkout: "/Checkout"
+  };
 
   return (
-    <div>
- <BrowserRouter>
-
-   
-      <Link class="nav-link" to={routs.home}>homePage</Link>
-      <Link class="nav-link" to={routs.product}>product</Link>
-      <Link class="nav-link" to={routs.ShoppingCart}>ShoppingCart</Link>
-      <Link class="nav-link" to={routs.Checkout}>Checkout</Link>
-
-       <Routes>
-        <Route path={routs.home} element={<HomePage/>} />
-        <Route path={routs.product} element={<ProductsPage/>} />
-        <Route path={routs.ShoppingCart} element={<ShoppingCartPage/>} />
-        <Route path={routs.Checkout} element={<CheckoutPage/>} />
-       </Routes>
-       </BrowserRouter>
-    </div>
-  )
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={routes.home}>Navbar</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to={routes.home}>HomePage</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={routes.ShoppingCart}>ShoppingCart</Link>
+            </li>
+            <li className="nav-item dropdown">
+              <Link className="nav-link dropdown-toggle" to={routes.product}>Product</Link>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={routes.Checkout}>Checkout</Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
+
