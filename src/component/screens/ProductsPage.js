4806d36f0e38465671ddb3dc345b0b14a5e8c36e
Your Name <you@example.com>
2025-05-06 16:40:02 +0300

    style hompage & navBar

--- src/component/screens/ProductsPage.js
+++ src/component/screens/ProductsPage.js
@@ -1,62 +1,30 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux';
-// import { useState } from 'react';
-// import ProductCard from '../Products/ProductCard';
-// import ProductModal from '../Products/ProductModal';
-
-// export default function ProductsPage(category) {
-//   const products=useSelector((state)=>state.productInventory.products);
-//   const [selectedProduct, setSelectedProduct] = useState(null);
-//   const Category=category;
-//   const handleProductClick = (product) => {
-//     setSelectedProduct(product);
-//   };
-
-//   const closeModal = () => {
-//     setSelectedProduct(null);
-//   };
-//   return (
-//     <div className="container mt-5">
-//   <div className="row">
-//     {products.map(product => ( 
-//      (!Category||Category===product.category)&&(<div key={product.id} className="col-md-4 mb-4">
-//         <ProductCard 
-//           product={product}
-//           onClick={() => handleProductClick(product)}
-//         />
-//       </div>)
-//     ))}
-//   </div>
-//   {selectedProduct && (
-//       <ProductModal 
-//         product={selectedProduct}
-//         onClose={closeModal}
-//       />
-//     )}
-// </div>
-
-//   )
-// }
 
 
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ProductCard from '../Products/ProductCard';
 import ProductModal from '../Products/ProductModal';
-import { useNavigate,useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 
 export default function ProductsPage() {
-  const location=useLocation();
-  const {category}=location.state||{};
+
+
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  const category = query.get('category') || '';
+  const subCategory = query.get('tat_category') || '';
+
+
+
   const routes = {
-    product:"/productModal"
+    product: "/productModal"
   };
 
-  const navigate=useNavigate();
-  
-  const goToProduct=(selectedProduct)=>{
-    navigate(routes.product,{state:{product:selectedProduct}})
+  const navigate = useNavigate();
+
+  const goToProduct = (selectedProduct) => {
+    navigate(routes.product, { state: { product: selectedProduct } })
 
   }
 
@@ -69,17 +37,22 @@ export default function ProductsPage() {
     setSelectedProduct(product);
   };
 
-  const closeModal = () => {
-    setSelectedProduct(null);
-  };
 
   return (
     <div className="container mt-5">
       <div className="row">
         {products
-          .filter((product) => !category || category === product.category)
+         .filter((product) => {
+          if (category && subCategory) {
+            return product.category === category && product.tat_category === subCategory;
+          } else if (category) {
+            return product.category === category;  
+          }
+          return true;  
+        })
+        
           .map((product) => (
-            <div key={product.id} className="col-md-4 mb-4">
+            <div key={product.id} className="col-md-3 mb-4">
               <ProductCard
                 product={product}
                 onClick={() => handleProductClick(product)}
@@ -88,12 +61,12 @@ export default function ProductsPage() {
           ))}
       </div>
 
-    
-  
-  {selectedProduct && (
-     goToProduct(selectedProduct)
-    )}
-</div>
+
+
+      {selectedProduct && (
+        goToProduct(selectedProduct)
+      )}
+    </div>
 
   )
 
