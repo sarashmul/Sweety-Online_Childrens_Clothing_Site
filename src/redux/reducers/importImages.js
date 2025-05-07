b72fa6f4e3f804038c42c5dd898f17c4ca14ee47
Your Name <you@example.com>
2025-05-07 03:55:56 +0300

    addImages

--- src/redux/reducers/importImages.js
+++ src/redux/reducers/importImages.js
@@ -1,3 +1,83 @@
 import shirtBoyWihte from '../images/shirtBoyWihte.jpg';
+import baby from '../images/baby.jpg';
+import baby2 from '../images/baby2.jpg';
+import baby3 from '../images/baby3.jpg';
+import baby4 from '../images/baby4.jpg';
+import baby5 from '../images/baby5.jpg';
+import baby6 from '../images/baby6.jpg';
+import boyBlackSuit from '../images/boyBlackSuit.webp';
+import BoyPants from '../images/BoyPants.jpg';
+import BoyPants2 from '../images/BoyPants2.jpg';
+import boyPassesSuit from '../images/boyPassesSuit.jpg';
+import Dress1 from '../images/Dress1.jpg';
+import dress3 from '../images/dress3.jpg';
+import dressStripedButtoned from '../images/dressStripedButtoned.jpg';
+import dressWhiteApron from '../images/dressWhiteApron.jpg';
+import shirtBoy from '../images/shirtBoy.jpg';
+import shirtBoy4 from '../images/shirtBoy4.jpg';
+import shirtBoy5 from '../images/shirtBoy5.jpg';
+import shirtBoy6 from '../images/shirtBoy6.jpg';
+import shirtBoy7 from '../images/shirtBoy7.jpg';
+import shirtBoy8 from '../images/shirtBoy8.jpg';
+import shirtBoyBrown from '../images/shirtBoyBrown.webp';
+import shirtBoyGreen from '../images/shirtBoyGreen.jpg';
+import shirtBoyLong from '../images/shirtBoyLong.jpg';
+import shirtBoyShort from '../images/shirtBoyShort.jpg';
+import shirtButtonPasses from '../images/shirtButtonPasses.jpg';
+import shirtGirl from '../images/shirtGirl.jpg';
+import shirtGirl2 from '../images/shirtGirl2.jpg';
+import shirtGirl4 from '../images/shirtGirl4.jpg';
+import shirtGir6 from '../images/shirtGirl5.jpg';
+import shirtGirlDub from '../images/shirtGirlDub.jpg';
+import shirtGirlLove from '../images/shirtGirlLove.jpg';
+import shirtGirlOrange from '../images/shirtGirlOrange.jpg';
+import swatterBoyWithButton from '../images/swatterBoyWithButton.jpg';
+import westBoy from '../images/westBoy.jpg';
+import westBoy3 from '../images/westBoy3.jpg';
 
-export const imagesarray=[shirtBoyWihte];
+
+export const imagesarray = [
+    // boy
+    shirtBoyWihte,
+    boyBlackSuit,
+    BoyPants,
+    BoyPants2,
+    boyPassesSuit,
+    shirtBoy,
+    shirtBoy4,
+    shirtBoy5,
+    shirtBoy6,
+    shirtBoy7,
+    shirtBoy8,
+    shirtBoyBrown,
+    shirtBoyGreen,
+    shirtBoyLong,
+    shirtBoyShort,
+    shirtButtonPasses,
+    swatterBoyWithButton,
+    westBoy,
+    westBoy3,
+
+    // dress
+    Dress1,
+    dress3,
+    dressStripedButtoned,
+    dressWhiteApron,
+
+    // girl
+    shirtGirl,
+    shirtGirl2,
+    shirtGirl4,
+    shirtGir6,
+    shirtGirlDub,
+    shirtGirlLove,
+    shirtGirlOrange,
+
+    // baby
+    baby,
+    baby2,
+    baby3,
+    baby4,
+    baby5,
+    baby6,
+];
